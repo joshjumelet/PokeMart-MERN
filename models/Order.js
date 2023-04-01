da2844e0b1f3a8c51cce0513240a1843433ac517
Joshua Jumelet <joshuajumelet@gmail.com>
2023-04-01 16:20:39 -0700

    build Order schema and model

---
+++ models/Order.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Order = new Schema(
+  {
+    user_id: { type: String, required: true },
+    products: [
+      {
+        product_id: {
+          type: String
+        },
+        quantity: {
+          type: Number,
+          default: 1
+        }
+      }
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: 'pending' }
+  },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model('Order', Order)
