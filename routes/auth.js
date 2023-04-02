42c59435d38fc724a39400ecf17ffc1617c50734
Joshua Jumelet <joshuajumelet@gmail.com>
2023-04-01 17:47:54 -0700

    build initial login function install jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -7,11 +7,36 @@ router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(req.body.password, 'Secret Passphrase')
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString()
   })
   try {
     const savedUser = await newUser.save()
     res.status(201).json(savedUser)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
+//Login User
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    !user && res.status(401).json('Incorrect username')
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    )
+    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8)
+    originalPassword !== req.body.password &&
+      res.status(401).json('Incorrect password')
+
+    const { password, ...others } = user._doc
+
+    res.status(200).json(others)
   } catch (error) {
     res.status(500).json(error)
   }
