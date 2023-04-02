096c42514b90321e57162c1b9ce1a20fcbf981a9
Joshua Jumelet <joshuajumelet@gmail.com>
2023-04-02 15:03:40 -0700

    build initial App.js axios for all products

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
 import './App.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  return <div className="App"></div>
+  const [products, setProducts] = useState([])
+
+  const GetProducts = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/api/products')
+      console.log(response.data)
+      setProducts(response.data)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  useEffect(() => {
+    GetProducts()
+  }, [])
+
+  return (
+    <div className="App">
+      <div>
+        <Nav />
+      </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home products={products} />} />
+        </Routes>
+      </main>
+    </div>
+  )
 }
 
 export default App
